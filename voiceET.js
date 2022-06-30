88247dbbe953f0258ffc12ed81e9098a2c0df2ef
dezid <93666818+dezid@users.noreply.github.com>
2022-06-30 12:37:17 +0100

    bvbvbv

--- voiceET.js
+++ voiceET.js
@@ -4,3 +4,7 @@ const navbarlinks = document.getElementsByClassName('navbar-links')[0]
 togglebutton.addEventListener('click',() => {
    navbarlinks.classList.toggle('active') 
 })
+var load = document.getElementById("loader-wrapper");
+window.addEventListener("load", function(){
+    load.style.display = "none";
+})
